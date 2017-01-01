b2418834132c437af5dd83d0e48408638be04383
Dmitry Dygalo <Stranger6667@users.noreply.github.com>
2017-01-01 12:13:56 +0100

    Fixed handling of `quoted-printable` payload. Fixes #76 (#77)

--- tests/django/test_backend.py
+++ tests/django/test_backend.py
@@ -65,8 +65,18 @@ def test_headers_encoding(patched_request):
     VERSION[:2] < (1, 7),
     reason='Django < 1.7 does not support `html_message` argument in `send_mail` function.'
 )
-def test_send_mail_html_message(patched_request):
-    send_mail(html_message='<html></html>', **SEND_KWARGS)
+@pytest.mark.parametrize('html_message', (
+    '<html></html>',
+    '''<html>
+       <body>
+           <div>
+             %s
+           </div>
+       </body>
+       </html>''' % ('.' * 1000)
+))
+def test_send_mail_html_message(html_message, patched_request):
+    send_mail(html_message=html_message, **SEND_KWARGS)
     assert patched_request.call_args[1]['json'] == ({
         'ReplyTo': None,
         'Subject': 'Subject here',
@@ -76,7 +86,28 @@ def test_send_mail_html_message(patched_request):
         'Cc': None,
         'Attachments': [],
         'TextBody': 'Here is the message.',
-        'HtmlBody': '<html></html>',
+        'HtmlBody': html_message,
+        'TrackOpens': False,
+        'Tag': None,
+        'From': 'sender@example.com'
+    }, )
+
+
+def test_send_long_text_line(patched_request):
+    kwargs = SEND_KWARGS.copy()
+    message = 'A' * 1000
+    kwargs['message'] = message
+    send_mail(**kwargs)
+    assert patched_request.call_args[1]['json'] == ({
+        'ReplyTo': None,
+        'Subject': 'Subject here',
+        'To': 'receiver@example.com',
+        'Bcc': None,
+        'Headers': [],
+        'Cc': None,
+        'Attachments': [],
+        'TextBody': message,
+        'HtmlBody': None,
         'TrackOpens': False,
         'Tag': None,
         'From': 'sender@example.com'
