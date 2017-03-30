59e4898e41ef425e1e34e731bc9e7faa68ede86a
Dmitry Dygalo <dadygalo@gmail.com>
2017-03-30 15:12:47 +0200

    Fix linting

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,7 +2,7 @@
 import pytest
 
 from postmarker.core import USER_AGENT, PostmarkClient
-from postmarker.models.messages import OutboundMessageManager, MessageManager
+from postmarker.models.messages import MessageManager, OutboundMessageManager
 from postmarker.models.triggers import TriggersManager
 
 
