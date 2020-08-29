d7b92a15756dbbad1d66cbe7b2ef25f680e59b10
Dmitry Dygalo <dadygalo@gmail.com>
2020-08-30 00:56:01 +0200

    chore: Drop another Python 2 shim

--- postmarker/pytest.py
+++ postmarker/pytest.py
@@ -8,8 +8,6 @@ from .core import PostmarkClient, requests
 @pytest.yield_fixture
 def postmark_request():
     """Mocks network requests to Postmark API."""
-    if patch is None:
-        raise AssertionError('To use pytest fixtures on Python 2, please, install postmarker["tests"]')
     with patch("postmarker.core.requests.Session.request", wraps=requests.Session().request) as patched:
         with patch("postmarker.core.requests.Session.send"):
             yield patched
