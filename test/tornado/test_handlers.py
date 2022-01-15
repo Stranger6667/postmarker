eedc5dae8ad1ee0b05cd80eae701c3675e88ea97
Dmitry Dygalo <dmitry@dygalo.dev>
2022-01-15 11:08:13 +0100

    test: Update dependencies

--- test/tornado/test_handlers.py
+++ test/tornado/test_handlers.py
@@ -41,10 +41,10 @@ class TestPostmarkMixin:
         self.assert_response(url, expected)
 
     def test_reuse_client(self):
-        self.set_postmark_response("")
+        self.set_postmark_response(b"")
         self.assert_response("/reuse/", b"True")
 
     def test_default_options(self):
         """Check if default values were used to initialize postmark client."""
-        self.set_postmark_response("")
+        self.set_postmark_response(b"")
         self.assert_response("/max_retries/", b"0")
